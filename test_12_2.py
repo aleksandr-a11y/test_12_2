ff3ba92f37ffb143105257fbc8c1d3a17942e827
ПК <@DESKTOP-H9D4HFD>
2024-11-17 22:10:37 +0700

    Добавьте файлы проекта.

---
+++ test_12_2.py
@@ -0,0 +1,42 @@
+﻿import runner_and_tournament
+import unittest
+
+
+class TournamentTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    def setUp(self):
+         self.runner_1 = runner_and_tournament.Runner('Усэйн', 10)
+         self.runner_2 = runner_and_tournament.Runner('Андрей', 9)
+         self.runner_3 = runner_and_tournament.Runner('Ник', 3)
+
+    @classmethod
+    def tearDownClass(cls):
+        for i in cls.all_results:
+            temp_dict = {}
+            for j in cls.all_results[i]:
+                temp_dict[j] = str(cls.all_results[i][j])
+            print(temp_dict)
+
+
+
+    def test_run_1(self):
+        tournament1 = runner_and_tournament.Tournament(90, self.runner_1, self.runner_3)
+        self.all_results[1] = tournament1.start()
+        self.assertTrue(self.all_results[1][max(self.all_results[1].keys())] == 'Ник')
+
+    def test_run_2(self):
+        tournament2 = runner_and_tournament.Tournament(90, self.runner_2, self.runner_3)
+        self.all_results[2] = tournament2.start()
+        self.assertTrue(self.all_results[2][max(self.all_results[2].keys())] == 'Ник')
+
+    def test_run_3(self):
+        tournament3 = runner_and_tournament.Tournament(90, self.runner_1, self.runner_2, self.runner_3)
+        self.all_results[3] = tournament3.start()
+        self.assertTrue(self.all_results[3][max(self.all_results[3].keys())] == 'Ник')
+
+
+if __name__ == "__main__":
+    unittest.main()
